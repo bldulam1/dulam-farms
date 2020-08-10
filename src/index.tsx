c414628d989ea1b84c89924035d24bf75545aa95
Brendon Dulam <brendondulam06@gmail.com>
2020-08-10 13:56:50 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import './index.css'
+
+import * as serviceWorker from './serviceWorker'
+
+import App from './components/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import * as serviceWorker from './serviceWorker'
-import App from './components/App/App'
 
 ReactDOM.render(
   <React.StrictMode>
