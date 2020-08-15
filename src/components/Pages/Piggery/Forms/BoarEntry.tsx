e35a57b1133dda19698c2beb8a39bd07e2410378
Brendon Dulam <brendondulam06@gmail.com>
2020-08-16 07:51:24 +0900

    boar breed

--- src/components/Pages/Piggery/Forms/BoarEntry.tsx
+++ src/components/Pages/Piggery/Forms/BoarEntry.tsx
@@ -6,21 +6,16 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
 import Grid from '@material-ui/core/Grid'
-import MenuItem from '@material-ui/core/MenuItem'
 import React from 'react'
-import ReactHookFormSelect from './ReactHookFormSelect'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import { yyyyMMdd } from './Forms.util'
 
 interface IHogEntry {
-  hogId: string
+  boarId: string
   birthDate: string
   recordDate: string
-  fatherPigID: string
-  motherPigID: string
-  sex: 'Male' | 'Female'
-  nipplesCount: number
+  breed: string
 }
 
 export default () => {
@@ -51,52 +46,23 @@ export default () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <CardHeader title="New Boar Form" />
         <CardContent>
-          <Typography variant="h6">Basic Info</Typography>
-
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6}>
+              <Typography variant="h6">Basic Info</Typography>
               <Controller
-                name="hogId"
-                label="Hog ID"
+                name="boarId"
+                label="Boar ID"
                 required
                 {...parentsControlProps}
               />
-              <ReactHookFormSelect
-                id="sex"
-                name="sex"
-                label="Sex"
-                control={control}
-                defaultValue="Female"
-                margin="normal"
-                fullWidth
-              >
-                <MenuItem value="Male">Male</MenuItem>
-                <MenuItem value="Female">Female</MenuItem>
-              </ReactHookFormSelect>
+              <Controller
+                name="breed"
+                label="Breed"
+                required
+                {...parentsControlProps}
+              />
             </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <Controller
-                name="nipplesCount"
-                label="Number of nipples"
-                type="number"
-                required
-                {...parentsControlProps}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="h6">Parents</Typography>
-              <Controller
-                name="fatherPigID"
-                label="Father Pig ID"
-                {...parentsControlProps}
-              />
-              <Controller
-                name="motherPigID"
-                label="Mother Pig ID"
-                {...parentsControlProps}
-              />
-            </Grid>
             <Grid item xs={12} sm={6}>
               <Typography variant="h6">Dates</Typography>
               <Controller
