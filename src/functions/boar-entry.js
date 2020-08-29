3ef85a1b3381cd57c319233caeff60419f14a56e
Brendon Dulam <brendondulam06@gmail.com>
2020-08-29 18:19:52 +0900

    refactor fiunction

--- src/functions/boar-entry.js
+++ src/functions/boar-entry.js
@@ -1,10 +1,9 @@
 const { errorResponse, successResponse } = require('../utils/lambda.utils')
 const { MongoClient } = require('mongodb')
 
-exports.handler = function (event, context, callback) {
+exports.handler = (event, context, callback) => {
   const { DB_URL } = process.env
   const DB_NAME = `farm-management${'development' ? '-dev' : ''}`
-
   const DB_COLLECTION = 'boars'
 
   if (!DB_URL) return errorResponse(callback, 'Database URL is empty')
