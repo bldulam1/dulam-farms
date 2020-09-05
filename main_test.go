08a079d676160305e43ed1b1f220dac9f421aa09
Brendon Dulam <brendondulam06@gmail.com>
2020-09-05 09:00:03 +0900

    initial commit

---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main_test
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	main "github.com/bldulam1/dulam-farms"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	tests := []struct {
+		request events.APIGatewayProxyRequest
+		expect  string
+		err     error
+	}{
+		{
+			// Test name has value
+			request: events.APIGatewayProxyRequest{QueryStringParameters: map[string]string{"name": "Paul"}},
+			expect:  "Hello Paul!",
+			err:     nil,
+		},
+		{
+			// Test name is null
+			request: events.APIGatewayProxyRequest{},
+			expect:  "Hello !",
+			err:     nil,
+		},
+	}
+
+	for i, test := range tests {
+		response, err := main.Handler(test.request)
+		assert.IsType(t, test.err, err)
+		assert.Equal(t, test.expect, response.Body)
+		log.Printf("Test %d: %s", i, response.Body)
+	}
+}
